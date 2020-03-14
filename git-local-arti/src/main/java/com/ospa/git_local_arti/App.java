cef6107342d5661515171fcd861154ef2623212d
Park, JongBum <jbpark03@gmail.com>
2020-03-14 09:33:08 +0900

    initial commit

---
+++ git-local-arti/src/main/java/com/ospa/git_local_arti/App.java
@@ -0,0 +1,13 @@
+package com.ospa.git_local_arti;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
